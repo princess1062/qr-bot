ee884c0f81268374dea348c4af0cabfd38806a08
princess1062 <puteri_anne@ymail.com>
2026-04-29 21:28:16 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,14 @@
-python-telegram-bot==21.6
-pillow
-opencv-python-headless
-pyzbar
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
+
+BOT_TOKEN = "8740908330:AAGh5BymbLksOzk999U_tsja6lVp3KsGQ1g"
+
+async def test(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot hidup ✅")
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT, test))
+    app.run_polling()
+
+main()
